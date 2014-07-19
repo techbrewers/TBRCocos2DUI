6ba2e465b72fa568df71c66f86d669290ef80f9c
Luciano Marisi <lucianomarisi@gmail.com>
2014-07-19 21:59:16 +0100

    Added initial example project

---
+++ Examples/TBRCocos2DUIExample/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TBRCocos2DUIExample
+//
+//  Created by Luciano Marisi on 19/07/2014.
+//  Copyright TechBrewers LTD. 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 TBRCocos2DUI
 ============
 
+[![Build Status](https://travis-ci.org/techbrewers/TBRCocos2DUI.svg)](https://travis-ci.org/techbrewers/TBRCocos2DUI)
+
 UI Components to be used with Cocos2D V3
